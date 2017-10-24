b9e2022c5479c5d03ea52183072b6fb9bc0e3e73
hesaiwang <hesaiwang@hesaiwangdeMacBook-Pro.local>
2017-10-24 14:06:58 +0800

    add some parser functions

--- src/js_object.js
+++ src/js_object.js
@@ -1,9 +1,11 @@
 /**
  * Created by shenwudi on 2017/9/25.
  */
+import {Token} from 'lexer.js'
 class JsObject{
     constructor(token){
         this.token = token
+        this.value = token.value
     }
     toString(){
         return this.token.value
@@ -13,6 +15,42 @@ class JsObject{
 class JsString extends JsObject{
     constructor(token){
         super(token)
+        this.length = token.value.length
+    }
+}
+
+class JsNumber extends JsObject{
+    constructor(token){
+        super(token)
+    }
+}
+
+class Statement extends JsObject{
+    constructor(token){
+        super(token)
+    }
+}
+
+class IfStatement extends Statement{
+    constructor(token,condtition,body){
+        super(token)
+        this.condtition = condtition
+        this.body = body
+    }
+}
+
+class ElseStatement extends Statement{
+    constructor(token,body){
+        super(token)
+        this.body = body
+    }
+}
+
+class ElseIfStatement extends Statement{
+    constructor(token,body){
+        super(token)
+        this.condtition = condtition
+        this.body = body
     }
 }
 
@@ -20,4 +58,8 @@ class JsString extends JsObject{
 export {
     JsObject,
     JsString,
+    JsNumber,
+    IfStatement,
+    ElseStatement,
+    ElseIfStatement,
 }
--- src/parser.js
+++ src/parser.js
@@ -2,14 +2,33 @@
  * Created by shenwudi on 2017/9/25.
  */
 import Lexer from './lexer.js'
-import { JsString } from './js_object.js'
+import { 
+    JsObject,
+    JsString,
+    JsNumber,
+    IfStatement,
+    ElseStatement,
+    ElseIfStatement, 
+} from './js_object.js'
 
-//获取class里的所有方法 Object.getOwnPropertyNames(Object.getPrototypeOf(this))，所有方法都是定义在原型上的
+//获取class里的所有方法 Object.getOwnPropertyNames(Object.getPrototypeOf(this))，如果所有方法都是定义在原型上的
 class Parser{
     constructor(tokens){
         this.tokens = tokens
         this.curr = 0
         this.elems = []
+        this.kws = Lexer.Keywords
+        this.ops = Lexer.Operator
+    }
+    //parser utils
+    match(tokenType){
+        if(this.tokens[this.curr].type == tokenType){
+            this.curr++
+            return true
+        }
+        else{
+            return false
+        }
     }
     parse(){
         var _this = this
@@ -41,40 +60,67 @@ class Parser{
         })
         console.log(_this.elems)
     }
+    // basic parsers
     parseString(){
         return new JsString(this.tokens[this.curr++])
     }
-    parseNumber(){}
+    parseNumber(){
+        return new JsNumber(this.tokens[this.curr++])
+    }
     parseIdent(){
         //func call
         //variable
     }
-    parseBreak (){}
-    parseElse (){}
-    parseNew (){}
-    parseVar (){}
-    parseTypeof (){}
-    parseCase (){}
-    parseFinally (){}
-    parseReturn (){}
-    parseVoid (){}
-    parseCatch (){}
-    parseIf (){}
-    parseWhile (){}
-    parseFor (){}
-    parseSwitch (){}
-    parseDefault (){}
-    parseThrow (){}
-    parseDelete (){}
-    parseIn (){}
-    parseTry (){}
-    parseDo (){}
-    parseInstanceof (){}
-    parseFunc (){}
-    parseUndefine (){}
-    parseNull (){}
-    parseTrue (){}
-    parseFalse (){}
+    parseBreak(){}
+    parseElse(){}
+    parseNew(){}
+    parseVar(){}
+    parseTypeof(){}
+    parseCase(){}
+    parseFinally(){}
+    parseReturn(){}
+    parseVoid(){}
+    parseCatch(){}
+    parseIf(){
+        var startToken = this.tokens[this.curr++]
+        match(this.kws.If)
+        this.curr++
+        match(this.ops.LBrace)
+        this.curr++
+        var exp = parseExpression()
+        this.curr++
+        var block
+        if(match(this.ops.LBracket)){
+            block = parseBlock()
+        }
+        return new IfStatement(startToken,exp,block)
+    }
+    parseWhile(){}
+    parseFor(){}
+    parseSwitch(){}
+    parseDefault(){}
+    parseThrow(){}
+    parseDelete(){}
+    parseIn(){}
+    parseTry(){}
+    parseDo(){}
+    parseInstanceof(){}
+    parseFunc(){}
+    parseUndefine(){}
+    parseNull(){}
+    parseTrue(){}
+    parseFalse(){}
+
+    //advance parsers
+    parseExpression(){
+
+    }
+    parseStatement(){
+
+    }
+    parseBlock(){
+
+    }
 }
 
 export default Parser
